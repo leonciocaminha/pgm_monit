1e62401b71d89fda9096c451204bad876db8ec80
Leo Caminha <leoncio.sobreira@gmail.com>
2015-08-04 21:24:02 -0300

    create_charts_controller

--- app/controllers/monit_controller.rb
+++ app/controllers/monit_controller.rb
@@ -5,4 +5,16 @@ class MonitController < ApplicationController
   def ammount_contrib
 
   end
+
+  def ammount_per_service
+  end
+
+  def reduction_percentual
+  end
+
+  def total_keeped
+  end
+
+  def biggest_dealers
+  end
 end
